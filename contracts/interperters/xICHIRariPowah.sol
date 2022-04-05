d82fb79414e145979c5fd947c0320082244de244
Mathuranath Metivier <Metiviermatt@live.com>
2022-04-05 13:32:41 -0500

    Xichi split into its own interperter

---
+++ contracts/interperters/xICHIRariPowah.sol
@@ -0,0 +1,43 @@
+ // SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.3;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+interface IStake {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+}
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+}
+
+
+contract xICHIRariPowah {
+    using SafeMath for uint256;
+
+    string public DESCRIPTION = "ICHIPowah Interperter for xichi tokens";
+
+    address public ICHIaddress = 0x903bEF1736CDdf2A537176cf3C64579C3867A881; //mainnet
+
+    function getSupply(address instance) public view returns (uint256 ichi) {
+        IStake stake = IStake(instance);
+        IERC20 ichiToken = IERC20(ICHIaddress);
+
+        ichi = ichiToken.balanceOf(address(stake));
+    }
+
+    function getPowah(address instance, address user, bytes32 params) public view returns (uint256 ichi) {
+        IStake stake = IStake(instance);
+        IERC20 ichiToken = IERC20(ICHIaddress);
+
+        //Rari xICHI
+        uint256 xICHI_totalICHI = ichiToken.balanceOf(address(stake));
+        uint256 rari_xICHI_balance = xICHIRariFA.balanceOfUnderlying(user);
+        uint256 rari_xICHI_total = xICHIRariFA.totalSupply();
+        
+        uint256 ichi = xICHI_totalICHI.mul(rari_xICHI_balance).div(rari_xICHI_total); 
+    }
+}
+
